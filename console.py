be26c347271f3040bf34d6118d3932f1a1f1b25b
BenFaruna <benjaminfaruna@gmail.com>
2022-08-07 22:31:58 +0100

    Added class_name.function capabilities for console

--- console.py
+++ console.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """module that contains entry point to the command interpreter"""
 import cmd
+import re
 import models
 
 
@@ -8,9 +9,33 @@ class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
 
     @staticmethod
+    def pattern(arg):
+        """
+        Retrieve the cmd method and the respective arguments according
+        to a pattern so that they can be invoked under this scheme:
+        <class>. <cmd> ([args, ...])
+        """
+        pattern = '\.([^.]+)\(|([^(),]+)[,\s()]*[,\s()]*'
+        argum = re.findall(pattern, arg)
+        cmd = argum[0][0]
+        argum = argum[1:]
+        line = ' '.join(map(lambda x: x[1].strip('"'), argum))
+        return cmd, line
+
+    @staticmethod
+    def pattern_handling(args):
+        """deals with the splitting and arrangement of argument"""
+        arg_list = []
+        PATTERN = re.compile(r''' (?=(?:[^'"]|'[^']*'|"[^"]*")*$)''')
+        new_args = PATTERN.split(args)
+        for arg in new_args:
+            arg_list.append(arg.strip('"'))
+        return arg_list
+
+    @staticmethod
     def handle_arg(arg):
         """return key and other arguments"""
-        new_arg = arg.split(" ")
+        new_arg = HBNBCommand.pattern_handling(arg)
         if len(new_arg) > 1:
             key = "{}.{}".format(new_arg[0], new_arg[1])
             return key, new_arg
@@ -139,6 +164,96 @@ class HBNBCommand(cmd.Cmd):
             if len(new_arg) == 1:
                 print("** instance id missing **")
                 return
+
+    def do_BaseModel(self, arg):
+        """
+        Lets you invoke each of the console methods
+        for the BaseModel class with the following syntax:
+        BaseModel.<cmd>([args, ...])
+
+        cmd can be: all, create, update, show destroy
+        """
+        cmd, line = self.pattern(arg)
+        self.onecmd(' '.join([cmd, 'BaseModel', line]))
+
+    def do_Amenity(self, arg):
+        """
+        Lets you invoke each of the console methods
+        for the Amenity class with the following syntax:
+        Amenity.<cmd>([args, ...])
+
+        cmd can be: all, create, update, show destroy
+        """
+        cmd, line = self.pattern(arg)
+        self.onecmd(' '.join([cmd, 'Amenity', line]))
+
+    def do_City(self, arg):
+        """
+        Lets you invoke each of the console methods
+        for the City class with the following syntax:
+        City.<cmd>([args, ...])
+
+        cmd can be: all, create, update, show destroy
+        """
+        cmd, line = self.pattern(arg)
+        self.onecmd(' '.join([cmd, 'City', line]))
+
+    def do_Place(self, arg):
+        """
+        Lets you invoke each of the console methods
+        for the Place class with the following syntax:
+        Place.<cmd>([args, ...])
+
+        cmd can be: all, create, update, show destroy
+        """
+        cmd, line = self.pattern(arg)
+        self.onecmd(' '.join([cmd, 'Place', line]))
+
+    def do_Review(self, arg):
+        """
+        Lets you invoke each of the console methods
+        for the Review class with the following syntax:
+        Review.<cmd>([args, ...])
+
+        cmd can be: all, create, update, show destroy
+        """
+        cmd, line = self.pattern(arg)
+        self.onecmd(' '.join([cmd, 'Review', line]))
+
+    def do_State(self, arg):
+        """
+        Lets you invoke each of the console methods
+        for the State class with the following syntax:
+        State.<cmd>([args, ...])
+
+        cmd can be: all, create, update, show destroy
+        """
+        cmd, line = self.self.pattern(arg)
+        self.onecmd(' '.join([cmd, 'State', line]))
+
+    def do_User(self, arg):
+        """
+        Lets you invoke each of the console methods
+        for the User class with the following syntax:
+        User.<cmd>([args, ...])
+
+        cmd can be: all, create, update, show destroy
+        """
+        cmd, line = self.pattern(arg)
+        self.onecmd(' '.join([cmd, 'User', line]))
+
+    def do_count(self, arg):
+        """
+        Retrieve the number of instances of a class: <class name>.count()
+        """
+        if len(arg) == 0:
+            print(len([str(value) for value in models.storage.all().values()]))
+        elif arg in models.classes:
+            print(len([str(value) for key, value in
+                       models.storage.all().items()
+                      if arg in key]))
+        else:
+            print("** class doesn't exist **")
 
 
 if __name__ == "__main__":
